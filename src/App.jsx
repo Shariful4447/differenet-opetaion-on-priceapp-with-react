2f6339d86646a3d132131c87b2768dd17c3a7370
Shariful4447 <shariful.islam4447@gmail.com>
2023-12-25 02:41:09 +0100

    project setup done with tailwind and daisy UI

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      
+      <h1 className='text-5xl'>Vite + React</h1>
+      <div className="avatar m-5">
+        <div className="w-24 rounded-full">
+          <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
